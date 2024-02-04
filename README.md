37baacf19b8e23cb8d489eb65d4e97719688cf6e
migdracios <migdracios@gmail.com>
2024-02-04 22:15:07 +0900

    Update get index template

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # eien
 간단하게 등록하는 나만의 생활습관 기록지
+
+## running method
+uvicorn main:app --reload
---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+
+app = FastAPI()
+
+# 정적 파일을 제공하기 위한 설정
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# 템플릿 엔진을 사용하기 위한 설정
+templates = Jinja2Templates(directory="templates")
+
+# index.html을 렌더링하는 엔드포인트
+@app.get("/")
+async def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# 이미지 파일 제공 예제
+@app.get("/images/{image_name}")
+async def get_image(image_name: str):
+    return FileResponse(f"static/images/{image_name}", media_type="image/png")
